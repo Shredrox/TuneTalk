9bfbaed42f199289ed6cb1253aa9e1a40d41be41
Shredrox <93082689+Shredrox@users.noreply.github.com>
2024-02-03 17:22:16 +0200

    Add tables to DbContext

--- TuneTalkServer/TuneTalk.Infrastructure/Data/TuneTalkDbContext.cs
+++ TuneTalkServer/TuneTalk.Infrastructure/Data/TuneTalkDbContext.cs
@@ -4,4 +4,13 @@ using TuneTalk.Core.Entities;
 
 namespace TuneTalk.Infrastructure.Data;
 
-public class TuneTalkDbContext(DbContextOptions options) : IdentityDbContext<User>(options);
+public class TuneTalkDbContext(DbContextOptions options) : IdentityDbContext<User>(options)
+{
+    public DbSet<Post> Posts { get; set; }
+    public DbSet<Like> Likes { get; set; }
+    public DbSet<Comment> Comments { get; set; }
+    public DbSet<Follow> Follows { get; set; }
+    public DbSet<Notification> Notifications { get; set; }
+    public DbSet<Chat> Chats { get; set; }
+    public DbSet<Message> Messages { get; set; }
+}
