2aa65ed9a6f8b521190848e38cda3ed811516309
Shredrox <93082689+Shredrox@users.noreply.github.com>
2024-02-07 19:38:52 +0200

    Update Header

--- TuneTalkClient/src/components/Header.tsx
+++ TuneTalkClient/src/components/Header.tsx
@@ -1,7 +1,20 @@
+import { useNavigate } from "react-router-dom";
+import useLogout from "../hooks/useLogout";
+
 const Header = () => {
+  const navigate = useNavigate();
+
+  const logout = useLogout();
+
+  const handleLogout = async () => {
+    await logout();
+    navigate('/');
+  }
+
   return (
-    <div className="bg-slate-700 text-white w-full flex justify-between items-center h-12 p-4 sticky top-0">
+    <div className="bg-[#18191b] text-white w-full flex justify-between items-center h-12 p-4 sticky top-0">
       TuneTalk
+      <button onClick={handleLogout}>Logout</button>
     </div>
   )
 }
