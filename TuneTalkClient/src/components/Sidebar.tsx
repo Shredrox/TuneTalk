1a5cad7ed7987a6382db2ee8aa86e105ee8e3b42
Shredrox <93082689+Shredrox@users.noreply.github.com>
2024-02-07 19:38:41 +0200

    Update Sidebar

--- TuneTalkClient/src/components/Sidebar.tsx
+++ TuneTalkClient/src/components/Sidebar.tsx
@@ -1,7 +1,39 @@
+import { Link, useLocation } from 'react-router-dom'
+import { IoHome } from "react-icons/io5";
+import { IoChatbubbles } from "react-icons/io5";
+
 const Sidebar = () => {
+  const location = useLocation();
+  
+  const isActive = (path : string) =>{
+    return location.pathname.includes(path) ? 
+    'bg-black' : 
+    ''
+  }
+
+  const linksData = [
+    { id: 1, to: '/home', text: 'Home', icon: <IoHome/> },
+    { id: 2, to: '/chats', text: 'Chats', icon: <IoChatbubbles/> }
+  ];
+
   return (
-    <div className="text-white bg-slate-800 h-screen sticky top-12 p-4">
-      Sidebar
+    <div className="text-white bg-[#0f1012] h-screen sticky top-12 p-4 w-[140px]">
+      <ul className='flex flex-col gap-2'>
+        {linksData.map((link) =>
+          <li key={link.id}>
+            <Link key={link.id} to={link.to}>
+              <button 
+                key={link.id} 
+                className={`flex items-center text-xl p-2 rounded gap-2
+                ${isActive(link.to)}`}
+                >
+                {link.icon}
+                {link.text}
+              </button>
+            </Link>
+          </li>
+        )}
+      </ul>
     </div>
   )
 }
