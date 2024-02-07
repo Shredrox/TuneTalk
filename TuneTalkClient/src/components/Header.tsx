48a6c7efda1effbd90b4320b4ece549245c38c91
Shredrox <93082689+Shredrox@users.noreply.github.com>
2024-02-07 18:22:16 +0200

    Create Header component

---
+++ TuneTalkClient/src/components/Header.tsx
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <div className="bg-slate-700 text-white w-full flex justify-between items-center h-12 p-4 sticky top-0">
+      TuneTalk
+    </div>
+  )
+}
+
+export default Header
