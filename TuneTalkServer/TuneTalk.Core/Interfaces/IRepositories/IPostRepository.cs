14718e1cd8553d11cb83655bab096b65eed22131
Shredrox <93082689+Shredrox@users.noreply.github.com>
2024-02-12 20:24:47 +0200

    Create PostRepository

---
+++ TuneTalkServer/TuneTalk.Core/Interfaces/IRepositories/IPostRepository.cs
@@ -0,0 +1,9 @@
+﻿using TuneTalk.Core.Entities;
+
+namespace TuneTalk.Core.Interfaces.IRepositories;
+
+public interface IPostRepository
+{
+    Task CreatePost(Post post);
+    Task<Post?> GetPostById(Guid id);
+}
---
+++ TuneTalkServer/TuneTalk.Infrastructure/Repositories/PostRepository.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using TuneTalk.Core.Entities;
+using TuneTalk.Core.Interfaces.IRepositories;
+using TuneTalk.Infrastructure.Data;
+
+namespace TuneTalk.Infrastructure.Repositories;
+
+public class PostRepository(TuneTalkDbContext context) : IPostRepository
+{
+    public async Task CreatePost(Post post)
+    {
+        context.Posts.Add(post);
+        await context.SaveChangesAsync();
+    }
+
+    public async Task<Post?> GetPostById(Guid id)
+    {
+        return await context.Posts
+            .FirstOrDefaultAsync(p => p.Id == id);
+    }
+}
