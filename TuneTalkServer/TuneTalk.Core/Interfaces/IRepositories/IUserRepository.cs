9ccc705b7c26c1c12046fa9d1bdbd7e7234493b4
Shredrox <93082689+Shredrox@users.noreply.github.com>
2024-02-12 20:25:04 +0200

    Create UserRepository

--- TuneTalkServer/TuneTalk.Core/Interfaces/IRepositories/IUserRepository.cs
+++ TuneTalkServer/TuneTalk.Core/Interfaces/IRepositories/IUserRepository.cs
@@ -1,6 +1,10 @@
-﻿namespace TuneTalk.Core.Interfaces.IRepositories;
+﻿using TuneTalk.Core.Entities;
+
+namespace TuneTalk.Core.Interfaces.IRepositories;
 
 public interface IUserRepository
 {
-    
+    Task<User?> GetUserByEmail(string email);
+    Task<User?> GetUserByName(string name);
+    Task CreateUser(User user, string password);
 }
--- TuneTalkServer/TuneTalk.Infrastructure/Repositories/UserRepository.cs
+++ TuneTalkServer/TuneTalk.Infrastructure/Repositories/UserRepository.cs
@@ -1,8 +1,23 @@
-﻿using TuneTalk.Core.Interfaces.IRepositories;
+﻿using Microsoft.AspNetCore.Identity;
+using TuneTalk.Core.Entities;
+using TuneTalk.Core.Interfaces.IRepositories;
 
 namespace TuneTalk.Infrastructure.Repositories;
 
-public class UserRepository : IUserRepository
+public class UserRepository(UserManager<User> userManager) : IUserRepository
 {
-    
+    public async Task<User?> GetUserByEmail(string email)
+    {
+        return await userManager.FindByEmailAsync(email);
+    }
+
+    public async Task<User?> GetUserByName(string name)
+    {
+        return await userManager.FindByNameAsync(name);
+    }
+
+    public async Task CreateUser(User user, string password)
+    {
+        await userManager.CreateAsync(user, password);
+    }
 }
