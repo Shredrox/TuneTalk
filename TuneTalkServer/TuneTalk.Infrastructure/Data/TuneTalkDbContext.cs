76f4654abb97d66c710a0a615ab7e324150c0a73
Shredrox <93082689+Shredrox@users.noreply.github.com>
2024-02-12 20:30:16 +0200

    Update TuneTalkDbContext.cs

--- TuneTalkServer/TuneTalk.Infrastructure/Data/TuneTalkDbContext.cs
+++ TuneTalkServer/TuneTalk.Infrastructure/Data/TuneTalkDbContext.cs
@@ -9,6 +9,9 @@ public class TuneTalkDbContext(DbContextOptions options) : IdentityDbContext<Use
     public DbSet<Post> Posts { get; set; }
     public DbSet<Like> Likes { get; set; }
     public DbSet<Comment> Comments { get; set; }
+    public DbSet<Tag> Tags { get; set; }
+    public DbSet<Playlist> Playlists { get; set; }
+    public DbSet<Song> Songs { get; set; }
     public DbSet<Follow> Follows { get; set; }
     public DbSet<Notification> Notifications { get; set; }
     public DbSet<Chat> Chats { get; set; }
