a0e29c8076600eac3d8ba9e0cd7d26c3387ea46b
Shredrox <93082689+Shredrox@users.noreply.github.com>
2024-02-15 16:39:54 +0200

    Add refresh token to User

--- TuneTalkServer/TuneTalk.Core/Entities/User.cs
+++ TuneTalkServer/TuneTalk.Core/Entities/User.cs
@@ -1,8 +1,11 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Identity;
 
 namespace TuneTalk.Core.Entities;
 
 public class User : IdentityUser
 {
-    
+    [MaxLength(256)]
+    public string? RefreshToken { get; set; }
+    public DateTime? RefreshTokenValidity { get; set; }
 }
