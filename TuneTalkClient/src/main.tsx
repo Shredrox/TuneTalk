1e0b37170283d38037e5b1405360a16b95c5c5b3
Shredrox <93082689+Shredrox@users.noreply.github.com>
2024-02-24 13:36:22 +0200

    Update css import in main

--- TuneTalkClient/src/main.tsx
+++ TuneTalkClient/src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import './styles/style.css'
+import './styles/global.css'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { AuthProvider } from './contexts/AuthContext.tsx'
 
