81f966a29a021f002a9c1ed10fde4440375d8ad8
Shredrox <93082689+Shredrox@users.noreply.github.com>
2024-02-24 17:00:35 +0200

    Update Header.tsx

--- TuneTalkClient/src/components/Header.tsx
+++ TuneTalkClient/src/components/Header.tsx
@@ -1,5 +1,19 @@
 import { useNavigate } from "react-router-dom";
 import useLogout from "../hooks/useLogout";
+import { ModeToggle } from "./ModeToggle";
+import { Avatar, AvatarFallback, AvatarImage } from "./Avatar";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/DropdownMenu"
+import {
+  LogOut,
+  User,
+} from "lucide-react"
 
 const Header = () => {
   const navigate = useNavigate();
@@ -12,11 +26,34 @@ const Header = () => {
   }
 
   return (
-    <div className="bg-[#18191b] text-white w-full flex justify-between items-center h-12 p-4 sticky top-0">
-      TuneTalk
-      <button onClick={() => navigate('/profile/hello')}>Profile</button>
-      <button onClick={handleLogout}>Logout</button>
-    </div>
+    <header className="bg-[#18191b] backdrop-blur 
+    border-b supports-[backdrop-filter]:bg-background/60 
+    w-full flex justify-between items-center h-12 p-4 fixed top-0 z-50">
+      <h3 className="scroll-m-20 text-2xl font-semibold tracking-tight">TuneTalk</h3>
+      <div className="flex gap-2">
+        <ModeToggle/>
+        <DropdownMenu>
+          <DropdownMenuTrigger>
+            <Avatar>
+              <AvatarImage src="https://github.com/shadcn.png"/>
+              <AvatarFallback>CN</AvatarFallback>
+            </Avatar>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent onCloseAutoFocus={(e) => e.preventDefault()}>
+            <DropdownMenuLabel>My Account</DropdownMenuLabel>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem onClick={() => navigate('/profile/hello')}>
+              <User className="mr-2 h-4 w-4" />
+              <span>Profile</span>
+            </DropdownMenuItem>
+            <DropdownMenuItem onClick={handleLogout}>
+              <LogOut className="mr-2 h-4 w-4" />
+              <span>Log out</span>
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </div>
+    </header>
   )
 }
 
