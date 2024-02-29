1dcb730567d107a2f92f35353f8190d03df957f9
Shredrox <93082689+Shredrox@users.noreply.github.com>
2024-02-29 16:24:25 +0200

    Create PostService

---
+++ TuneTalkServer/TuneTalk.Core/Interfaces/IServices/IPostService.cs
@@ -0,0 +1,8 @@
+﻿using TuneTalk.Core.DTOs.Requests.Post;
+
+namespace TuneTalk.Core.Interfaces.IServices;
+
+public interface IPostService
+{
+    Task CreatePost(CreatePostRequest post);
+}
---
+++ TuneTalkServer/TuneTalk.Core/Services/PostService.cs
@@ -0,0 +1,20 @@
+﻿using TuneTalk.Core.DTOs.Requests.Post;
+using TuneTalk.Core.Entities;
+using TuneTalk.Core.Interfaces.IRepositories;
+using TuneTalk.Core.Interfaces.IServices;
+
+namespace TuneTalk.Core.Services;
+
+public class PostService(IPostRepository postRepository) : IPostService
+{
+    public async Task CreatePost(CreatePostRequest request)
+    {
+        var newPost = new Post
+        {
+            Title = request.Name,
+            Content = request.Content
+        };
+        
+        await postRepository.CreatePost(newPost);
+    }
+}
