51e0c01a5542c87bc48637db27dd19796a2374f5
Shredrox <93082689+Shredrox@users.noreply.github.com>
2024-03-07 11:54:21 +0200

    Create UserController

---
+++ TuneTalkServer/TuneTalk.API/Controllers/UserController.cs
@@ -0,0 +1,30 @@
+using Microsoft.AspNetCore.Mvc;
+using TuneTalk.Core.DTOs.Responses.User;
+using TuneTalk.Core.Exceptions;
+using TuneTalk.Core.Interfaces.IServices;
+
+namespace TuneTalk.API.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class UserController(IUserService userService) : ControllerBase
+{
+    [HttpGet("{search}")]
+    public async Task<IActionResult> GetUsersBySearch(string search)
+    {
+        try
+        {
+            var users = await userService.SearchByName(search);
+            var response = users
+                .Select(user => new UserSearchResultResponse(user))
+                .ToList();
+
+            return Ok(response);
+        }
+        catch (NotFoundException e)
+        {
+            Console.WriteLine(e);
+            return NotFound();
+        }
+    }
+}
