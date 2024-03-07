27065b42974426e2f0e98ba77066d5025d900049
Shredrox <93082689+Shredrox@users.noreply.github.com>
2024-03-07 22:31:07 +0200

    Create Loading component

---
+++ TuneTalkClient/src/components/fallback/Loading.tsx
@@ -0,0 +1,11 @@
+const Loading = () => {
+  return (
+    <div>
+      <span className="loader bg-secondary-foreground 
+      after:bg-secondary-foreground 
+      before:bg-secondary-foreground"></span>
+    </div>
+  )
+}
+
+export default Loading
