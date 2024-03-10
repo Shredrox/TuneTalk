78e9349a0057ad2e99149dfcfe5379c2b282231f
Shredrox <93082689+Shredrox@users.noreply.github.com>
2024-03-10 21:25:04 +0200

    Update home

--- TuneTalkClient/src/components/home/Feed.tsx
+++ TuneTalkClient/src/components/home/Feed.tsx
@@ -1,7 +1,61 @@
+import { Avatar, AvatarFallback, AvatarImage } from "../Avatar";
+import { Badge } from "../Badge";
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "../Card"
+import { FaHeart, FaRegHeart, FaCommentAlt } from "react-icons/fa";
+
 const Feed = () => {
   return (
-    <div className="bg-[#0f1012] p-4 w-[650px] min-h-screen h-full rounded-2xl">
-      Feed
+    <div className="bg-[#0f1012] p-4 w-[650px] min-h-fit rounded-2xl flex flex-col gap-4">
+      {Array.from({length: 5}).map((_,index) =>(
+      <Card key={index} className="min-h-56 cursor-pointer">
+        <CardHeader>
+          <CardTitle>Periphery - Dying Star</CardTitle>
+          <CardDescription className="flex items-center gap-1">
+            <Avatar className="w-6 h-6">
+              <AvatarImage src="https://github.com/shadcn.png"/>
+              <AvatarFallback>CN</AvatarFallback>
+            </Avatar>
+            <p className="text-primary-foreground">Meesho</p>
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <img className="w-full h-[300px] object-cover rounded" src="https://picsum.photos/1000/500" alt="" />
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+            Etiam non tincidunt velit, non efficitur leo.
+            Praesent non volutpat justo. Morbi ut lectus porta, 
+            faucibus nibh vehicula, tincidunt lacus. 
+            Ut congue mattis mauris, ut placerat nunc 
+            lacinia vitae. Nunc mattis 
+            ligula non magna blandit, ac condimentum tortor tristique. 
+            Mauris hendrerit et magna vitae eleifend. Pellentesque in est ipsum. 
+            Aliquam non enim eu nibh posuere varius ac at ex. 
+            Vivamus ornare tempor orci, id ornare nulla malesuada eu. 
+            Pellentesque faucibus enim non bibendum laoreet. 
+          </p>
+          <div>
+            <Badge className="mr-1 mb-1">Metal</Badge>
+            <Badge className="mr-1">Progressive</Badge>
+            <Badge className="mr-1">Progressive</Badge>
+            <Badge className="mr-1">Progressive</Badge>
+            <Badge className="mr-1">Progressive</Badge>
+            <Badge className="mr-1 mb-1">Metal</Badge>
+            <Badge className="mr-1">Progressive</Badge>
+            <Badge className="mr-1">Progressive</Badge>
+            <Badge className="mr-1">Progressive</Badge>
+          </div>
+        </CardContent>
+        <CardFooter>
+          <div className="flex gap-4">
+            <div className="flex items-center gap-2">
+              <FaRegHeart className="w-5 h-5 cursor-pointer"/> Like
+            </div>
+            <div className="flex items-center gap-2">
+              <FaCommentAlt className="w-5 h-5 cursor-pointer"/> Comment
+            </div>
+          </div>
+        </CardFooter>
+      </Card>
+      ))}
     </div>
   )
 }
--- TuneTalkClient/src/pages/Home.tsx
+++ TuneTalkClient/src/pages/Home.tsx
@@ -1,9 +1,50 @@
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/Carousel"
+
 import Feed from "../components/home/Feed"
+import { Card, CardContent } from "@/components/Card"
+import { IoIosTrendingUp } from "react-icons/io";
+import CreatePostDialog from "@/components/home/CreatePostDialog";
+
 
 const Home = () => {
   return (
     <div className="text-white flex">
-      <Feed/>
+      <div className="flex flex-col gap-4">
+        <CreatePostDialog/>
+        <Feed/>
+      </div>
+      
+      <div className="px-4 w-[600px] fixed right-0 flex flex-col gap-4">
+        <div className="pl-5 flex items-center gap-1">
+          <IoIosTrendingUp className="w-6 h-full"/>
+          <h3 className="scroll-m-20 text-2xl font-semibold tracking-tight">Trending</h3>
+        </div>
+        
+        <Carousel className="ml-11 w-full max-w-sm">
+          <CarouselContent className="-ml-1">
+            {Array.from({ length: 5 }).map((_, index) => (
+              <CarouselItem key={index} className="pl-1 md:basis-1/2 lg:basis-1/3">
+                <div className="p-1 flex flex-col">
+                  <Card>
+                    <CardContent className="flex aspect-square items-center justify-center p-6">
+                      Image
+                    </CardContent>
+                  </Card>
+                  <span className="text-2xl font-semibold">Song {index + 1}</span>
+                </div>
+              </CarouselItem>
+            ))}
+          </CarouselContent>
+          <CarouselPrevious />
+          <CarouselNext />
+        </Carousel>
+      </div>
     </div>
   )
 }
