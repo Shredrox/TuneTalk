4b6d90dd87c30acbb8ede14a3fde713124a5f7e7
Shredrox <93082689+Shredrox@users.noreply.github.com>
2024-03-10 15:19:36 +0200

    Update MainLayout.tsx

--- TuneTalkClient/src/layouts/MainLayout.tsx
+++ TuneTalkClient/src/layouts/MainLayout.tsx
@@ -8,7 +8,7 @@ const MainLayout = () => {
       <Header/>
       <div className='min-h-screen'>
         <Sidebar/>
-        <main className='ml-[150px] mt-12 p-6'>
+        <main className='ml-[150px] mt-16 p-6'>
           <Outlet />
         </main>
       </div>
