623709b60621835e4c81aeceaa8ddb250534c3e5
Shredrox <93082689+Shredrox@users.noreply.github.com>
2024-03-10 15:15:19 +0200

    Create TagRepository

---
+++ TuneTalkServer/TuneTalk.Core/Interfaces/IRepositories/ITagRepository.cs
@@ -0,0 +1,9 @@
+﻿using TuneTalk.Core.Entities;
+
+namespace TuneTalk.Core.Interfaces.IRepositories;
+
+public interface ITagRepository
+{
+    Task CreateTag(Tag tag);
+    Task<List<Tag>> GetAllTags();
+}
---
+++ TuneTalkServer/TuneTalk.Infrastructure/Repositories/TagRepository.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TuneTalk.Core.Entities;
+using TuneTalk.Core.Interfaces.IRepositories;
+using TuneTalk.Infrastructure.Data;
+
+namespace TuneTalk.Infrastructure.Repositories;
+
+public class TagRepository(TuneTalkDbContext context) : ITagRepository
+{
+    public async Task CreateTag(Tag tag)
+    {
+        context.Tags.Add(tag);
+        await context.SaveChangesAsync();
+    }
+
+    public async Task<List<Tag>> GetAllTags()
+    {
+        return await context.Tags.ToListAsync();
+    }
+}
