fee34a375ca4b8de79f0b70c66ae8e50a181b04a
Shredrox <93082689+Shredrox@users.noreply.github.com>
2024-03-14 18:34:50 +0200

    Update Post entity

--- TuneTalkServer/TuneTalk.Core/Entities/Post.cs
+++ TuneTalkServer/TuneTalk.Core/Entities/Post.cs
@@ -5,8 +5,13 @@ public class Post
     public Guid Id { get; set; }
     public required string Title { get; set; }
     public required string Content { get; set; }
-    public List<Tag> Tags { get; set; }
+    public ICollection<Tag> Tags { get; set; }
     public DateTime CreatedAt { get; set; }
     public string UserId { get; set; }
     public User User { get; set; }
+
+    public Post()
+    {
+        Tags = new List<Tag>();
+    }
 }
