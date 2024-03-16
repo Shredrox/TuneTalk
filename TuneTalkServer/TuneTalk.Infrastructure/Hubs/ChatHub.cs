fc5a9f01cf8e197b83a459236def7a5c7d91a63e
Shredrox <93082689+Shredrox@users.noreply.github.com>
2024-03-16 18:26:47 +0200

    Create ChatHub

---
+++ TuneTalkServer/TuneTalk.Infrastructure/Hubs/ChatHub.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using TuneTalk.Core.Interfaces.IClients;
+
+namespace TuneTalk.Infrastructure.Hubs;
+
+public class ChatHub : Hub<IChatClient>
+{
+    public async Task SendMessage(string user, string message)
+    {
+        await Clients.All.ReceiveMessage(user, message);
+    }
+    
+    public async Task SendMessageToGroup(string sender, string receiver, string message)
+    {
+        await Clients.Group(receiver).ReceiveMessage(sender, message);
+    } 
+}
