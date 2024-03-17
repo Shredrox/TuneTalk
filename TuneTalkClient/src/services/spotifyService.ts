ed3381e5bc3ba14e27a7ef596e87da5993d67c2c
Shredrox <93082689+Shredrox@users.noreply.github.com>
2024-03-17 10:57:33 +0200

    Add getSpotifySongsBySearch to spotifyService

--- TuneTalkClient/src/services/spotifyService.ts
+++ TuneTalkClient/src/services/spotifyService.ts
@@ -1,7 +1,13 @@
+import SearchSong from "@/interfaces/spotify/SearchSong";
 import axios from "../axios/axios";
 import SpotifyProfile from "../interfaces/spotify/SpotifyProfile";
 
 export const getSpotifyProfile = async () : Promise<SpotifyProfile> =>{
   const response = await axios.get('/Spotify/profile');
+  return response.data;
+}
+
+export const getSpotifySongsBySearch = async (search : string) : Promise<SearchSong[]> =>{
+  const response = await axios.get(`/Spotify/search/${search}`);
   return response.data;
 }
